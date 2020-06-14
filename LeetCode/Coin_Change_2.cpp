02842192f4c2e2954213e4d96592b27e25a31374
Hardik <hardikkhandelwal500@gmail.com>
2020-06-15 00:21:05 +0530

    [add] soln link

--- LeetCode/Coin_Change_2.cpp
+++ LeetCode/Coin_Change_2.cpp
@@ -29,47 +29,58 @@ using namespace std;
 
 //FIXED (faster) Top-Down
 //FIXME (Still TLE)
-int get_change_dp(vector<int> coins, int amount, int index, vector<vector<int>> &dp)
-{
-    if (amount == 0)
-    {
-        return 1;
-    }
+// int get_change_dp(vector<int> coins, int amount, int index, vector<vector<int>> &dp)
+// {
+//     if (amount == 0)
+//     {
+//         return 1;
+//     }
 
-    if (dp[amount][index]) return dp[amount][index] - 1;
+//     if (dp[amount][index]) return dp[amount][index] - 1;
 
-    int result = 0;
+//     int result = 0;
 
-    for (int i = index; i < coins.size(); i++)
-    {
-        if (amount >= coins[i])
-        {
-            result += get_change_dp(coins, amount - coins[i], i, dp);
-        }
-    }
+//     for (int i = index; i < coins.size(); i++)
+//     {
+//         if (amount >= coins[i])
+//         {
+//             result += get_change_dp(coins, amount - coins[i], i, dp);
+//         }
+//     }
 
-    //save the result at this index with this amount so that in future we need not calcaulate it again
-    dp[amount][index] = result + 1;
-    return dp[amount][index] - 1;
-}
+//     //save the result at this index with this amount so that in future we need not calcaulate it again
+//     dp[amount][index] = result + 1;
+//     return dp[amount][index] - 1;
+// }
 
 int main()
 {
-    vector<int> coins = {1, 2, 5};
-    int amount = 5;
+    vector<int> coins = {1, 2, 3};
+    int amount = 3;
 
-    // cout << get_change(coins, 5, 0);
+    //First Solution
+    // cout << get_change(coins, amount, 0);
 
-    int n = coins.size();
-    vector<vector<int>> dp(amount + 1, vector<int>(n + 1, 0));
+    //Second Solution
 
-    cout<<get_change_dp(coins, 5, 0, dp);
+    // int n = coins.size();
+    // vector<vector<int>> dp(amount + 1, vector<int>(n + 1, 0));
 
-    //Fast
-    // vector<int> dp(amount + 1, 0);
-    // dp[0] = 1;
-    // for (int i = 0; i < coins.size(); i++)
-    //     for (int j = coins[i]; j <= amount; j++)
-    //         dp[j] = dp[j - coins[i]] + dp[j];
-    // cout << dp.back();
+    // cout<<get_change_dp(coins, amount, 0, dp);
+
+    //Third Solution (Fastest)
+    vector<int> dp(amount + 1, 0);
+    dp[0] = 1;
+    for (int i = 0; i < coins.size(); i++){
+        for (int j = coins[i]; j <= amount; j++){
+            int d = dp[j];
+            int e = coins[i];
+            int f = dp[j - coins[i]];
+            dp[j] = dp[j - coins[i]] + dp[j];
+        }
+    }
+    cout << dp.back();
 }
+
+//LINK: for solution explaination
+//https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/discuss/677345/C++-DP-top-down-bottom-up-space-optimized-bottom-up
