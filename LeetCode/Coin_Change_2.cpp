6ac63b47cc827d7af36dfa76e661ed3e8f73a9f5
Hardik <hardikkhandelwal500@gmail.com>
2020-06-14 21:33:25 +0530

    [add] coin change 2 son (slow)

---
+++ LeetCode/Coin_Change_2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//FIXME (Time limit exceeded)
+// int get_change(int arr[], int sum = 5, int index = 0)
+// {
+
+//     if (sum == 0)
+//     {
+//         return 1;
+//     }
+//     else if (sum < 0)
+//     {
+//         return 0;
+//     }
+//     int result = 0;
+
+//     for (int i = index; i < 3; i++)
+//     {
+//         if (sum >= arr[i])
+//         {
+//             result += get_change(arr, sum - arr[i], i);
+//         }
+//     }
+
+//     return result;
+// }
+
+int main()
+{
+    int arr[] = {1, 2, 5};
+
+    // cout << get_change(arr, 5, 0);
+}
