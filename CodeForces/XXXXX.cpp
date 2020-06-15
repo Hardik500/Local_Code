3d9c070a6c64515dc3aeec0b470258163ee8777b
Hardik <hardikkhandelwal500@gmail.com>
2020-06-15 19:59:15 +0530

    [add] codeforces xxxxx soln (bit copied)

--- CodeForces/XXXXX.cpp
+++ CodeForces/XXXXX.cpp
@@ -1,39 +1,64 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void first(){
-    int n,x, count = 0, sum = 0;
+void first()
+{
+    int n, x, count = 0, sum = 0, left = 0, right = 0, ans = 0;
 
-    cin>>n>>x;
+    cin >> n >> x;
 
-    int a[x];
+    int arr[n];
+    right = n - 1;
 
-    for(int i = 0;i<n; i++){
-        cin>>a[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
 
-        if(a[i] % x == 0){
+        if (arr[i] % x == 0)
+        {
             count += 1;
         }
-
-        sum += a[i];
     }
 
-    if(sum % x == 0 and count == n){
-        cout<<-1<<"\n";
-    }
-    else{
-        cout<<"0"<<"\n";
+    if (sum % x == 0 and count == n)
+        ans = -1;
+    else
+    {
+        while (left < right + 1)
+        {
+            sum -= arr[left];
+            left += 1;
+            if (sum % x)
+            {
+                ans = max(ans, left);
+            }
+        }
+
+        left = 0;
+        sum = 0;
+
+        while (left < right + 1)
+        {
+            sum -= arr[right];
+            if (sum % x)
+            {
+                ans = max(ans, n - right);
+            }
+            right -= 1;
+        }
     }
 
+    cout << ans << "\n";
 }
 
-int main(){
+int main()
+{
     int count = 0;
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> count;
 
-    cin>>count;
-
-    while(count--){
+    while (count--)
         first();
-    }
 }
