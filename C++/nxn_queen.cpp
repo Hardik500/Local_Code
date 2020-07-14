f826eb405c5869fec15ca34fd466a34fc593f982
Hardik <hardikkhandelwal500@gmail.com>
2020-07-14 13:00:08 +0530

    [add] NXN Queen [backtracking]

--- C++/nxn_queen.cpp
+++ C++/nxn_queen.cpp
@@ -2,59 +2,82 @@
 
 using namespace std;
 
-int n = 4;
+const int N = 4;
+
+void printSolution(int chess_board[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << chess_board[i][j] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+
+bool isSafe(int chess_board[N][N], int row, int col)
+{
+    int i, j; 
+  
+    for (i = 0; i < col; i++) 
+        if (chess_board[row][i]) 
+            return false; 
+  
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+        if (chess_board[i][j]) 
+            return false; 
+
+    for (i = row, j = col; j >= 0 && i < N; i++, j--) 
+        if (chess_board[i][j]) 
+            return false; 
+  
+    return true;
+}
+
+int solveNQUtil(int chess_board[N][N], int col = 0)
+{
+    if(col >= N)
+        return 1;
+    
+    for (int i = 0; i < N; i++)
+    {
+        if (isSafe(chess_board, i, col))
+        {
+            chess_board[i][col] = 1;
+
+            if(solveNQUtil(chess_board, col + 1)){
+                return 1;
+            }
+            else{
+                chess_board[i][col] = 0;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void solveNQ()
+{
+    int chess_board[N][N];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            chess_board[i][j] = 0;
+        }
+    }
+
+    if (solveNQUtil(chess_board))
+        printSolution(chess_board);
+    else
+        cout << "Nope\n";    
+}
 
 int main()
 {
-    int A[n][n], count = 0;
-    bool diagonal = false;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            A[i][j] = 0;
-        }
-    }
-
-    for (int i1 = 0; i1 < n; i1++)
-    {
-        for (int j1 = 0; j1 < n; j1++)
-        {
-            A[i1][j1] = 1;
-
-            diagonal = false;
-
-            for (int i2 = 0; i2 < n; i2++)
-            {
-                for (int j2 = 0; j2 < n; j2++)
-                {
-                    if (A[i2][j2] != 1)
-                    {
-                        if (i1 == j1 and i2 == j2)
-                        {
-                            diagonal = false;
-                        }
-                        else
-                        {
-                            diagonal = true;
-                        }
-
-                        if(i1 == j2 and j1 == i2){
-                            diagonal = false;
-                        }
-
-                        if (i1 != i2 and j1 != j2 and diagonal)
-                        {
-                            count += 1;
-                        }
-                    }
-                }
-            }
-
-            A[i1][j1] = 0;
-        }
-    }
-
-    cout << count << "\n";
+    solveNQ();
 }
