a679087c9172742301395b5a616ebd13bac2c10f
Hardik <hardikkhandelwal500@gmail.com>
2020-09-11 23:10:18 +0530

    [add] another solution to find k closest elements

--- LeetCode/Find_K_Closest_Elements.cpp
+++ LeetCode/Find_K_Closest_Elements.cpp
@@ -16,6 +16,13 @@ vector<int> findClosestElements(vector<int> &A, int k, int x)
     return vector<int>(A.begin() + left, A.begin() + left + k);
 }
 
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int index = std::lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int i = index - 1, j = index;                                    
+        while(k--) (i<0 || (j<arr.size() && abs(arr[i] - x) > abs(arr[j] - x) ))? j++: i--;
+        return vector<int>(arr.begin() + i + 1, arr.begin() + j );
+    }
+
 int main()
 {
     vector<int> arr = {3, 4, 5, 6, 7, 8};
